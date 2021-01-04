8745d17157222ac262cf9b65085fa9cd7aa6f73a
bigyank <this.is.blueorange@gmail.com>
2021-01-04 18:44:47 +0545

    added mini map on details page view

---
+++ src/Components/PlaceDetailMap.js
@@ -0,0 +1,36 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvent,
+} from "react-leaflet";
+import { Box, Paper, makeStyles } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+
+const PlaceDetailMap = ({ location }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+  return (
+    <Box m={matches ? 4 : 0}>
+      <Paper>
+        <MapContainer
+          style={{ height: "40vh", width: "100%" }}
+          center={location}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+            url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERNAME}/${process.env.REACT_APP_MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`}
+          />
+          <Marker position={location} />
+        </MapContainer>
+      </Paper>
+    </Box>
+  );
+};
+
+export default PlaceDetailMap;
--- src/Pages/PlaceDetailPage.js
+++ src/Pages/PlaceDetailPage.js
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { CircularProgress } from "@material-ui/core";
 import PlaceDetailHeader from "../Components/PlaceDetailHeader";
+import PlaceDetailMap from "../Components/PlaceDetailMap";
 import { getPlaceDetail } from "../services/place";
 
 const PlaceDetail = ({ match }) => {
@@ -11,10 +12,12 @@ const PlaceDetail = ({ match }) => {
   });
 
   if (isLoading) return <CircularProgress />;
+  if (!data) return <CircularProgress />;
 
   return (
     <>
       <PlaceDetailHeader data={data} />
+      <PlaceDetailMap location={data.location} />
     </>
   );
 };
