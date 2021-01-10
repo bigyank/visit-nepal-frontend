755f94c6a140c4c649c514cfcb3d5238e82e8e2c
bigyank <this.is.blueorange@gmail.com>
2021-01-10 22:09:11 +0545

    chnaged google oauth url according to process.env

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -19,7 +19,10 @@ const Login = () => {
   const classes = useStyles();
 
   const handleGoogleOauth = () => {
-    window.location.href = "http://localhost:4000/api/auth/google";
+    window.location.href =
+      process.env.REACT_APP_ENV === "development"
+        ? "http://localhost:4000/api/auth/google"
+        : "http://vnepal.me/api/auth/google";
   };
 
   return (
--- src/Pages/SignupPage.js
+++ src/Pages/SignupPage.js
@@ -18,7 +18,10 @@ const Signup = () => {
   const classes = useStyles();
 
   const handleGoogleOauth = () => {
-    window.location.href = "http://localhost:4000/api/auth/google";
+    window.location.href =
+      process.env.REACT_APP_ENV === "development"
+        ? "http://localhost:4000/api/auth/google"
+        : "http://vnepal.me/api/auth/google";
   };
 
   return (
